f891af5160e9766bbcc4cc8d4f181dc3a713737f
yaros <yachek008@gmail.com>
2019-02-23 15:24:08 +0200

    functional components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Navbar} from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from "./shared/dishes";
+import Main from './components/MainComponent';
+
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          dishes: DISHES
-      };
-  }
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <Navbar href='/'>Ristorante Con Fusion</Navbar>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}/>
+      <div className='App'>
+        <Main/>
       </div>
     );
   }
